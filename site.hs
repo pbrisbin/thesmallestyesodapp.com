c7fd222121d000b4f4ffbfb53cd3c01fd764a5b2
patrick brisbin <pbrisbin@gmail.com>
2020-02-26 08:59:29 -0500

    Update dependencies

--- site.hs
+++ site.hs
@@ -1,16 +1,19 @@
 module Main where
+import Data.FileEmbed
+import Data.ByteString.Char8 (unpack)
 import Yesod
 import Network.Wai.Handler.Warp (run)
 import System.Environment
 import Text.Highlighting.Kate
 
-mkYesod "()" [parseRoutes| / R GET |]
-instance Yesod ()
+data App = App
+mkYesod "App" [parseRoutes| / R GET |]
+instance Yesod App
 getR = (\c -> defaultLayout $ setTitle "The Smallest Yesod App" >>
     [whamlet|
         <style>#{preEscapedToMarkup $ styleToCss pygments}
         <h3>I am The Smallest Yesod App!
         <p>Here is my source code:#{c}
         <a href="https://github.com/pbrisbin/thesmallestyesodapp.com">Make me smaller
-    |]) =<< (liftIO $ fmap (formatHtmlBlock defaultFormatOpts {numberLines = True} . highlightAs "haskell") $ readFile "./site.hs")
-main = getEnv "PORT" >>= \p -> toWaiApp () >>= run (read p)
+    |]) $ formatHtmlBlock defaultFormatOpts {numberLines = True} $ highlightAs "haskell" $ unpack $(embedFile "./site.hs")
+main = getEnv "PORT" >>= \p -> toWaiApp App >>= run (read p)
